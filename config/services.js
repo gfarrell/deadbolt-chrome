7db41e4ee7010c6f09bcf196e293cd197e44b11c
Gideon Farrell <me@gideonfarrell.co.uk>
2014-08-08 15:23:52 +0100

    Configured twitter

--- config/services.js
+++ config/services.js
@@ -2,5 +2,9 @@ define({
     'facebook': {
         'urls': [/(.+\.)?facebook\.com/],
         'redirect': 'https://www.facebook.com'
+    },
+    'twitter': {
+        urls: [/(.+\.)?twitter\.com/],
+        redirect: 'https://twitter.com'
     }
 });
